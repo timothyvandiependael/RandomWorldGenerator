7675d2dce315638ca098f02ffc037790a9b4d969
timothyvandiependael <timothy.vandiependael@gmail.com>
2016-11-15 17:35:34 +0100

    Second commit

--- RandomWorldGenerator/Logic.cs
+++ RandomWorldGenerator/Logic.cs
@@ -258,7 +258,6 @@ namespace RandomWorldGenerator
                         goY += 1;
                     sourceDirection = 0;
                     break;
-
                 case 3:
                     grens = 0;
                     if (goX != grens)
